e78880b37114d0db0a4e213a96341524da8f7487
lvbaoxin <745458741@qq.com>
2021-09-24 11:07:41 +0800

    no message

--- mobile/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from mobile.views import index
-
-urlpatterns = [
-    path('', index.index, name="mobile_index")
-]
--- web/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from web.views import index
-
-urlpatterns = [
-    path('', index.index, name="web_index")
-]
